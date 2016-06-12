e46c8ea2b0e43b14b72706a6e16ed315725fcc14
Scott Kildall <scott@kildall.com>
2016-06-12 11:48:34 -0700

    code-cleansing

--- MagicLantern.ino
+++ MagicLantern.ino
@@ -7,6 +7,8 @@
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 Adafruit_7segment matrix = Adafruit_7segment();
 
+//-- 1 = send debug messages to the serial port, 0 = no debug (faster)
+#define SERIAL_DEBUG (1)
 
 #include <MP3Trigger.h>
 MP3Trigger trigger;
@@ -18,40 +20,65 @@ int trackNum = 1;
 //-- general
 int debounceMS = 100;
 
-//
-int switchPin = 12;
+//-- pins
+int soundTriggerPin = 12;         // a momentary switch, used to trigger the sound for debugging
 
 SoftwareSerial trigSerial = SoftwareSerial(2, 3);
 
 void setup() {
-  pinMode(switchPin, INPUT);  // Your switch pin (though technically speaking, this code is not needed)
+  if( SERIAL_DEBUG ) {
+    Serial.begin(9600);
+    Serial.println("Magic Lantern, starting up");
+  }
 
-   
-  Serial.begin(9600);
+  //-- initialize the 4-segment number display
   matrix.begin(0x70);
   matrix.print(0);
   matrix.writeDisplay();
   
-  // Start serial communication with the trigger (over Serial)
-  // hard serial
+  //-- initalize various pins, specify INPUT pins for code legibiity
+  pinMode(soundTriggerPin, INPUT); 
+
   
+  
+  
+  
+  // Start serial communication with the trigger (over Soft Serial)
+  //-- Use 19200 baud, note this requires a .INI file
   trigger.setup(&trigSerial);
   trigSerial.begin( 19200 );
+  delay(1000);
 
-  //trigger.setup(&Serial);
-  //Serial.begin( MP3Trigger::serialRate() );
-  // Start looping TRACK001.MP3
-  //trigger.setLooping(true,1);
-  randomSeed(A0);
 
-  //numTracks = getNumTracks();
-  numTracks = 17;
-   matrix.print(numTracks);
+  
+  //-- get the number of tracks
+  numTracks = getNumTracks();
+  //numTracks = 17;
+  
+  if( numTracks == -1 ) {
+     matrix.print(9999);
+     numTracks = 0;
+  }
+  else
+      matrix.print(numTracks);
+  
+   
    matrix.writeDisplay();
+
+  if( SERIAL_DEBUG ) {
+    Serial.print("num tracks = " );
+    Serial.print( numTracks );    
+    Serial.println();
+  }
+  
+   //-- seed random number generator
+   randomSeed(A0);
+
+   
 }
 
 void loop() {
-  // process signals from the trigger
+  // process signals from the trigger [OBSOLETE?]
   //trigger.update();
 
   //-- tester, don't use with button
@@ -65,12 +92,17 @@ void loop() {
   }
 }
 
+//-- return true if we are to trigger a sound, false if not; will change the states accordingly
 boolean triggerSound() {
-  if( digitalRead(switchPin) == true  ) {   // button is pressed
+  //-- check to see if the floor mat has been triggered
+  //-- [code to go here]
+  
+  //-- check the momentary sound trigger pin
+  if( digitalRead(soundTriggerPin) == true  ) {   // button is pressed
         delay(debounceMS); // debounce
          
          // wait for button to be released
-         while( digitalRead(switchPin) == true ) {
+         while( digitalRead(soundTriggerPin) == true ) {
            ;  // do nothing
          }
          delay(debounceMS); // debounce
@@ -80,6 +112,7 @@ boolean triggerSound() {
   return false;
 }
 
+//-- OBSOLETE CODE
 void playRandomTrack() {
   if( startTime + waitTime < millis() ) {
     trackNum = random(numTracks) + 1;
@@ -95,76 +128,93 @@ void playRandomTrack() {
   }
 }
 
-/*
 
-void loop() {
- 
-  // put your main code here, to run repeatedly:
-
-  // send data only when you receive data:
-  if (Serial.available() > 0)
-    readMP3Data();        
-}
-
-
-void readMP3Data() {
-  numTracks = Serial.read();
-
-matrix.print(numTracks);
-  matrix.writeDisplay();
-  // say what you got:
-//  Serial.print("I received: ");
- // Serial.println(incomingByte, DEC);
-        
-}
-*/
 //-- send num tracks query
 //-- fix, won't work for more than 99 tracks
 //-- also, gives
+//-- return -1 if error
 int getNumTracks() {
+  //-- an array of numerical entries, which will be reverse-ordered
+  int numArrayEntries = 4;
+  int numArray[numArrayEntries];        // max 3 entries on this MP3 card, we will do 4 just in case
+  for( int i = 0; i < numArrayEntries; i++ ) {
+    numArray[i] = -1;   // flag
+  }
+  
+  //-- make request
   trigSerial.flush();
+  //trigSerial.println("S1");
+  
   trigSerial.write('S');
   trigSerial.write('1');
-  delay(100);
-
-  int count = 0;
-  int num = 0;
+  delay(1000);
   
-   Serial.println("getting num tracks");
+  int byteCount = 0;      //-- How many bytes we have counted, use for multiplier, 
+  int num = 0;            //-- number of tracks, aggregating
+
+  if( SERIAL_DEBUG ) {
+    Serial.println("getNumTracks()");
+  }
+
+   if( !trigSerial.available() ) {
+      if( SERIAL_DEBUG ) {
+          Serial.println("no software serial response");
+      }
+  }
+  
   while( trigSerial.available() )
   {
     int incomingByte = trigSerial.read();
-    if( count == 0 ) {
+
+    //-- first byte should be ASCII 61
+    if( byteCount == 0 ) {
       if (incomingByte != 61 ) {
-        Serial.print("ERROR");
+        if( SERIAL_DEBUG )
+          Serial.print("ERROR: expecting first byte of 61");
+
+         return -1;
       }
-      
-      //break;
     }
+
+    //-- next bytes are actual numbers
     else {
       // shift over that many decimal places
       incomingByte = incomingByte - 48;
 
-Serial.println();
-      Serial.print(incomingByte, DEC);
-
-     // crude convert to int
-      Serial.print(count, DEC);
-      num = num * (10* (count-1));
-      num += incomingByte;
-
-
-       Serial.println();
-           Serial.print(num, DEC);
-           
+      if( SERIAL_DEBUG ) {
         Serial.println();
+        Serial.print(incomingByte, DEC);
+      }
+      
+     // crude convert to int
+      Serial.print(byteCount, DEC);
+      Serial.println();
+      
+      //-- check for array overflow (shoudn't happen)
+      int numArrayIndex = byteCount - 1;
+      if( numArrayIndex > (numArrayEntries-1) )  {
+        if( SERIAL_DEBUG )
+          Serial.print("ERROR: more tracks than expected");
+         return -1;
+      }
+
+      // how many digits we have processed: 10 to that power
+      numArray[numArrayIndex] = incomingByte;
+    
+      if( SERIAL_DEBUG ) { 
+           Serial.print(numArray[numArrayIndex], DEC);
+           Serial.println();
+      }
+     
     }
 
-    Serial.println("incr");
-    count++;
+    Serial.println("incr"); 
+    byteCount++;
   }
 
-  return num/2;
+  //-- now, reverse-process the array
+  
+  return 17;
 }
   
 
